d669f21b42510b750f08ee3acf888be5cbf46aba
eric.hsu <eric.hsu@snapask.com>
2021-11-22 02:02:25 +0800

    refactor: remove unused codes

--- VideoHumanDetection/VideoCapture.swift
+++ VideoHumanDetection/VideoCapture.swift
@@ -57,49 +57,6 @@ class VideoCapture: NSObject {
         }
     }
 
-//    func setUpCamera(sessionPreset: AVCaptureSession.Preset) -> Bool {
-//        captureSession.beginConfiguration()
-//        captureSession.sessionPreset = sessionPreset
-//
-//        guard let captureDevice = AVCaptureDevice.default(for: AVMediaType.video) else {
-//            print("Error: no video devices available")
-//            return false
-//        }
-//
-//        guard let videoInput = try? AVCaptureDeviceInput(device: captureDevice) else {
-//            print("Error: could not create AVCaptureDeviceInput")
-//            return false
-//        }
-//
-//        if captureSession.canAddInput(videoInput) {
-//            captureSession.addInput(videoInput)
-//        }
-//
-//        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-//        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspect
-//        previewLayer.connection?.videoOrientation = .portrait
-//        self.previewLayer = previewLayer
-//
-//        let settings: [String: Any] = [
-//            kCVPixelBufferPixelFormatTypeKey as String: NSNumber(value: kCVPixelFormatType_32BGRA)
-//        ]
-//
-//        videoOutput.videoSettings = settings
-//        videoOutput.alwaysDiscardsLateVideoFrames = true
-//        videoOutput.setSampleBufferDelegate(self, queue: queue)
-//        if captureSession.canAddOutput(videoOutput) {
-//            captureSession.addOutput(videoOutput)
-//        }
-//
-//        // We want the buffers to be in portrait orientation otherwise they are
-//        // rotated by 90 degrees. Need to set this _after_ addOutput()!
-//        videoOutput.connection(with: AVMediaType.video)?.videoOrientation = .portrait
-//
-//        captureSession.commitConfiguration()
-//
-//        return true
-//    }
-
     // MARK: Private
 
     private func setupAsset(_ asset: AVAsset?) -> Bool {
